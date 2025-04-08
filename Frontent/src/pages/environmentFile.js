59923e45c3e87c083b3a151cd7cfc59cb3197400
rohitsrivastava2000 <bhanusrivastavadlw@gmail.com>
2025-04-08 20:05:01 +0530

    add backend url

--- Frontent/src/pages/environmentFile.js
+++ Frontent/src/pages/environmentFile.js
@@ -1,5 +1,5 @@
 let IS_PROD=true;
-const server=IS_PROD?"https://video-meet-5618.onrender.com":"http://localhost:5000"
+const server=IS_PROD?"https://video-meet-backend-22.onrender.com":"http://localhost:5000"
 
 console.log("")
 export default server;
